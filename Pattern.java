802d381f81af162d7ce843ac8223ab73b10181d4
JitendraBordiya <67721067+JitendraBordiya@users.noreply.github.com>
2021-10-22 14:08:12 +0530

    Java Programs

---
+++ Pattern.java
@@ -0,0 +1,34 @@
+package Com.Programs;
+
+import java.util.Scanner;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter The Value of n : ");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+                 {
+			
+			for(int j=1;j<=i;j++)
+                   {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+                      for(int i=1;i<=n;i++) {
+
+			
+			for(int j=1;j<=n-i;j++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
